4c2fd20bf8c40d966a2e3b9cdf8f22f80963a486
Margid2024 <vihangp@zignuts.com>
2024-08-31 15:53:36 +0530

    ssdsdiisjd

--- src/components/student/Edit.vue
+++ src/components/student/Edit.vue
@@ -6,12 +6,7 @@
  <template>
   jdjjdjd
   <h1 class="text-3xl font-bold underline">
-<<<<<<< HEAD
  Edit Student
-=======
- Edit Studentjdjjdjdjjjdjd
- ddjjdjd
->>>>>>> aba51a3 (heloo123)
   </h1>
 </template>
 
